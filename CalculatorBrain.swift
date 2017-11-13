0578fd4799873fa9235f0349e393ea69b1005543
en2455926 <32452471+en2455926@users.noreply.github.com>
2017-11-13 22:06:37 +0100

    Add files via upload
    Functional Change Sign and cos function added.

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -10,23 +10,59 @@
 
 import Foundation
 
+
+func changeSign(operand: Double) -> Double {
+    return -operand
+}
+
+func multiply(op1: Double, op2: Double) -> Double {
+    return op1 * op2
+}
+
 struct CalculatorBrain {
 
     private var accumulator: Double?
+ 
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double,Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary <String,Operation> = [
+        "pii" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "sqrt" : Operation.unaryOperation(sqrt),
+        "cos" : Operation.unaryOperation(cos),
+        "+/-" : Operation.unaryOperation(changeSign),
+        "*" : Operation.binaryOperation(multiply),
+        "=" : Operation.equals
+        ]
     
     mutating func performOperation(_ symbol:String) {
-        switch symbol {
-        case "pii":
-        accumulator = Double.pi
-        case "sqrt":
-            if let operand = accumulator {
-                accumulator = sqrt(operand)
+        if let operation = operations[symbol] {
+            switch operation {
+            case .constant(let Value):
+                accumulator = Value
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    accumulator = function(accumulator!)
+                }
+            case . binaryOperation(let function):
+                break
+            case.equals:
+                break
             }
-        default:
-            break
         }
 }
 
+    private struct PendingBinaryOperation {
+        let function: (Double,Double) -> Double
+        let firstOperand: Double
+        
+    }
+    
     mutating func setOperand(_ operand: Double) {
         accumulator = operand
     }
